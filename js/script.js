0566de089eb32c205b1328c955057c4f8ead9e88
Leonardo Griesang <griesang.leonardo@outlook.com>
2023-08-07 09:47:58 -0300

    js de exercicios

--- js/script.js
+++ js/script.js
@@ -15,22 +15,47 @@ function ativaexercicios() {
 }  
 ativaexercicios();  
 
-const icones = document.querySelectorAll('.icones i');
-const conteudoReceita = document.querySelectorAll('.js-conteudo-receita')
 
-function ativaIcon(i, index) {
-    conteudoReceita.forEach((conteudo) => {
-        conteudo.classList.remove('ativo');
+function ativaReceitas() {
+
+    const icones = document.querySelectorAll('.icones i');
+    const conteudoReceita = document.querySelectorAll('.js-conteudo-receita')
+
+    if(icones.length && conteudoReceita.length) {
+
+        function ativaIcon(i, index) {
+            conteudoReceita.forEach((conteudo) => {
+                conteudo.classList.remove('ativo');
+            })
+            conteudoReceita[index].classList.add('ativo');
+            icones.forEach((i) => {
+                i.classList.remove('ativo')
+            })
+            i.classList.add('ativo')
+        }
+
+        icones.forEach((i, index) => {
+            i.addEventListener('click', () => {
+                ativaIcon(i, index)
+            })
+        })
+    }    
+}
+ativaReceitas();
+
+const listaExer = document.querySelectorAll('.list-exercicios li');
+const exer = document.querySelectorAll('.js-exercicios');
+
+exer[0].classList.add('ativo');
+function ativaConteudoE(index) {
+    exer.forEach((exer) => {
+        exer.classList.remove('ativo');
     })
-    conteudoReceita[index].classList.add('ativo');
-    icones.forEach((i) => {
-        i.classList.remove('ativo')
-    })
-    i.classList.add('ativo')
+    exer[index].classList.add('ativo');
 }
 
-icones.forEach((i, index) => {
-    i.addEventListener('click', () => {
-        ativaIcon(i, index)
-    })
+listaExer.forEach((Lexer, index) => {
+    Lexer.addEventListener('click', () => {
+        ativaConteudoE(index)
+    });
 })
